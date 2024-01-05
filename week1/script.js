4320992f5874aecdeda420cefb66c4170b789bc4
miketma <noreply>
2024-01-04 17:43:50 -0800

    +semicolons appended in script.js +src of script.js in index.html +inline script started for statement

--- week1/script.js
+++ week1/script.js
@@ -1,4 +1,4 @@
-let firstName = "Mike"
-let lastName = "Ma"
-let yearsOfStudy = "three quarters"
-let goal = "get a job in industry"
+let firstName = "Mike";
+let lastName = "Ma";
+let yearsOfStudy = "three quarters";
+let goal = "get a job in industry";
