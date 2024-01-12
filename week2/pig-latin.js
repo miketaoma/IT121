93de49ed6a3aab2aa816479af4c396622cbc7dbc
miketma <noreply>
2024-01-11 19:24:53 -0800

    +.split to place userInput into array +for loop multiple word translations +reference to input @ i instead of user input directly

--- week2/pig-latin.js
+++ week2/pig-latin.js
@@ -1,24 +1,29 @@
 /* Author: Mike Ma
 1/11/24 */
 
-let inputText = prompt("Please enter a word in English:");
-let firstChar = inputText.slice(0, 1); //saving parts of input that we want to test to variables.
-let secondChar = inputText.slice(1, 2);
-let firstTwoChar = inputText.slice(0, 2);
-firstChar = firstChar.toLowerCase(); //using .toLowerCase() in order to make the code case agnostic.
-secondChar = secondChar.toLowerCase();
-firstTwoChar = firstTwoChar.toLowerCase();
-let translatedText; //declaring blank variable where translated pig latin will be stored and printed from.
+let inputText = prompt("Please enter a word or phrase in English. Phrase format should be \"word1 word2 word3\"");
+let inputTextArray = inputText.split(" "); //splitting input on the space
 
-if (inputText.length < 2) { //tests if user input is a single letter or a blank, produces error if true.
-    alert("Error! Your input is too short!");
-} else if (firstChar == "a" || firstChar == "e" || firstChar == "i" || firstChar == "o" || firstChar == "u") { //tests if first character of user input is a vowel, appends "way" if true.
-    translatedText = inputText + "way";
-    document.write(translatedText);
-} else if (secondChar == "a" || secondChar == "e" || secondChar == "i" || secondChar == "o" || secondChar == "u") { //tests if second character of user input is a vowel (if test above fails we know first char is a consonant), appends that consonant + "ay" if true.
-    translatedText = inputText.slice(1) + firstChar + "ay";
-    document.write(translatedText);
-} else { //if both tests above fail, that means first two letters are consonants, appends those consonants + "ay".
-    translatedText = inputText.slice(2) + firstTwoChar + "ay";
-    document.write(translatedText);
+for (let i = 0; i < inputTextArray.length; i++) { //for loop iterates the if statements below through each element of array.
+    
+    let firstChar = inputTextArray[i].slice(0, 1); //saving parts of input that we want to test to variables.
+    let secondChar = inputTextArray[i].slice(1, 2);
+    firstChar = firstChar.toLowerCase(); //using .toLowerCase() in order to make the code case agnostic.
+    secondChar = secondChar.toLowerCase();
+    let translatedText; //declaring blank variable where translated pig latin will be stored and printed from.
+
+    if (inputTextArray[i].length < 2) { //tests if user input is a single letter or a blank, produces error if true.
+        alert("Error! Your input is too short!");
+    } else if (firstChar == "a" || firstChar == "e" || firstChar == "i" || firstChar == "o" || firstChar == "u") { //tests if first character of user input is a vowel, appends "way" if true.
+        translatedText = inputTextArray[i] + "way";
+        document.write(translatedText);
+    } else if (secondChar == "a" || secondChar == "e" || secondChar == "i" || secondChar == "o" || secondChar == "u" || secondChar == "y") { //tests if second character of user input is a vowel (if test above fails we know first char is a consonant), appends that consonant + "ay" if true. Including "y" bec of edgecase words like "by" or "mystery"
+        translatedText = inputTextArray[i].slice(1) + firstChar + "ay";
+        document.write(translatedText);
+    } else { //if both tests above fail, that means first two letters are consonants, appends those consonants + "ay".
+        translatedText = inputTextArray[i].slice(2) + firstChar + secondChar + "ay";
+        document.write(translatedText);
+    }
+
+    document.write(" "); //places space between each pig latin translation
 }
