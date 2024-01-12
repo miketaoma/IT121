73ebd83bd30733a4b63fcda03aba5cd01662bc17
miketma <noreply>
2024-01-11 18:17:46 -0800

    +one consonant test case +two consonant test case

--- week2/pig-latin.js
+++ week2/pig-latin.js
@@ -3,8 +3,10 @@
 
 let inputText = prompt("Please enter a word in English:");
 let firstChar = inputText.slice(0, 1);
+let secondChar = inputText.slice(1, 2);
 let firstTwoChar = inputText.slice(0, 2);
 firstChar = firstChar.toLowerCase();
+secondChar = secondChar.toLowerCase();
 firstTwoChar = firstTwoChar.toLowerCase();
 let translatedText;
 
@@ -13,7 +15,13 @@ if (inputText.length < 2) {
 } else if (firstChar == "a" || firstChar == "e" || firstChar == "i" || firstChar == "o" || firstChar == "u") {
     translatedText = inputText + "way";
     document.write(translatedText);
+} else if (secondChar == "a" || secondChar == "e" || secondChar == "i" || secondChar == "o" || secondChar == "u") {
+    translatedText = inputText.slice(1) + firstChar + "ay";
+    document.write(translatedText);
+} else {
+    translatedText = inputText.slice(2) + firstTwoChar + "ay";
+    document.write(translatedText);
 }
 
 
-//document.write(firstTwoChar);
+//document.write(secondChar);
