409e6273d2c4905d870b75c4a62b57be6e2a6bfd
miketma <noreply>
2024-01-11 17:08:56 -0800

    +variables +conditional block cases:     error input too shorrt     first char = vowel

--- week2/pig-latin.js
+++ week2/pig-latin.js
@@ -1,3 +1,19 @@
 /* Author: Mike Ma
 1/11/24 */
 
+let inputText = prompt("Please enter text");
+let firstChar = inputText.slice(0, 1);
+let firstTwoChar = inputText.slice(0, 2);
+firstChar = firstChar.toLowerCase();
+firstTwoChar = firstTwoChar.toLowerCase();
+let translatedText;
+
+if (inputText.length < 2) {
+    alert("Error! Your input is too short!");
+} else if (firstChar == "a" || firstChar == "e" || firstChar == "i" || firstChar == "o" || firstChar == "u") {
+    translatedText = inputText + "way";
+    document.write(translatedText);
+}
+
+
+//document.write(firstTwoChar);
