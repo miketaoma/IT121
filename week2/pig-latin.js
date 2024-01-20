ecc8952ea7bcd61f00ca2c24d6ad9130dad995cd
miketma <noreply>
2024-01-20 12:17:48 -0800

    changed tests to use string of vowels instead of ind chars

--- week2/pig-latin.js
+++ week2/pig-latin.js
@@ -6,6 +6,7 @@ let inputTextArray = inputText.split(" "); //splitting input on the space
 
 for (let i = 0; i < inputTextArray.length; i++) { //for loop iterates the if statements below through each element of array.
     
+    let vowels = "aeiou";
     let firstChar = inputTextArray[i].slice(0, 1); //saving parts of input that we want to test to variables.
     let secondChar = inputTextArray[i].slice(1, 2);
     firstChar = firstChar.toLowerCase(); //using .toLowerCase() in order to make the code case agnostic.
@@ -14,10 +15,10 @@ for (let i = 0; i < inputTextArray.length; i++) { //for loop iterates the if sta
 
     if (inputTextArray[i].length < 2) { //tests if user input is a single letter or a blank, produces error if true.
         alert("Error! Your input is too short!");
-    } else if (firstChar == "a" || firstChar == "e" || firstChar == "i" || firstChar == "o" || firstChar == "u") { //tests if first character of user input is a vowel, appends "way" if true.
+    } else if (vowels.includes(firstChar)) { //tests if first character of user input is a vowel, appends "way" if true.
         translatedText = inputTextArray[i] + "way";
         document.write(translatedText);
-    } else if (secondChar == "a" || secondChar == "e" || secondChar == "i" || secondChar == "o" || secondChar == "u" || secondChar == "y") { //tests if second character of user input is a vowel (if test above fails we know first char is a consonant), appends that consonant + "ay" if true. Including "y" bec of edgecase words like "by" or "mystery"
+    } else if (vowels.includes(secondChar)) { //tests if second character of user input is a vowel (if test above fails we know first char is a consonant), appends that consonant + "ay" if true. Including "y" bec of edgecase words like "by" or "mystery"
         translatedText = inputTextArray[i].slice(1) + firstChar + "ay";
         document.write(translatedText);
     } else { //if both tests above fail, that means first two letters are consonants, appends those consonants + "ay".
